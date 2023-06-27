27a8c2e07c09f8bced7ad8114b0e319c6494f850
miguel <a18829@alunos.ipca.pt>
2023-06-27 20:37:53 +0100

    add auth Logic and routing

--- ezcharge-server/src/app.module.ts
+++ ezcharge-server/src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentMethodModule } from './payment-method/payment-method.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ReservationModule } from './reservation/reservation.module';
     PaymentMethodModule,
     TransactionModule,
     ReservationModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- ezcharge-server/src/charging-station/charging-station.controller.ts
+++ ezcharge-server/src/charging-station/charging-station.controller.ts
@@ -6,12 +6,15 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { ChargingStationService } from './charging-station.service';
 import { ChargingStation } from './schemas/charging-station.schema';
 import { ApiTags, ApiOperation, ApiParam, ApiBody } from '@nestjs/swagger';
 import { CreateChargingStationDto } from './dto/create-charging-station.dto';
 import { UpdateChargingStationDto } from './dto/update-charging-station.dto';
+import { AdminGuard } from '../auth/admin-guard/admin.guard';
+import { UserGuard } from '../auth/user/user.guard';
 
 @Controller('charging-stations')
 @ApiTags('Charging Stations')
@@ -19,12 +22,14 @@ export class ChargingStationController {
   constructor(private chargingStationService: ChargingStationService) {}
 
   @Get()
+  @UseGuards(UserGuard)
   @ApiOperation({ summary: 'Get all charging stations' })
   findAll(): Promise<ChargingStation[]> {
     return this.chargingStationService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(UserGuard)
   @ApiOperation({ summary: 'Get a charging station by ID' })
   @ApiParam({ name: 'id', description: 'Charging Station ID' })
   findById(@Param('id') id: string): Promise<ChargingStation> {
@@ -32,6 +37,7 @@ export class ChargingStationController {
   }
 
   @Post()
+  @UseGuards(AdminGuard)
   @ApiOperation({ summary: 'Create a new charging station' })
   @ApiBody({ type: CreateChargingStationDto })
   create(
@@ -41,6 +47,7 @@ export class ChargingStationController {
   }
 
   @Patch(':id')
+  @UseGuards(AdminGuard)
   @ApiOperation({ summary: 'Update a charging station' })
   @ApiParam({ name: 'id', description: 'Charging Station ID' })
   @ApiBody({ type: UpdateChargingStationDto })
@@ -52,6 +59,7 @@ export class ChargingStationController {
   }
 
   @Delete(':id')
+  @UseGuards(AdminGuard)
   @ApiOperation({ summary: 'Delete a charging station' })
   @ApiParam({ name: 'id', description: 'Charging Station ID' })
   delete(@Param('id') id: string): Promise<ChargingStation> {
