5196d0ba5001d5c19f5fb9c823d738f2c2b8ac96
miguel <a18829@alunos.ipca.pt>
2023-07-04 19:50:50 +0100

    update firebase messaging

--- ezcharge-server/src/notification/firebase.service.ts
+++ ezcharge-server/src/notification/firebase.service.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 @Injectable()
 export class FirebaseService {
   private readonly firebaseUrl: string = 'https://fcm.googleapis.com/fcm/send';
-  private readonly serverKey: string = 'XXXTOKENXXX'; // You need to replace it with your actual server key
+  private readonly serverKey: string =
+    'AAAARmezrxM:APA91bE_sOzOWQ7KuatA_PAuU2ITqlwIsN9AKw9cgy6fx5ihdXHW6OZSYgGbkzkHI1A5CYrBcCcchPVkKsp588-aSS6p1fWy2wEUSAIZ-C6lyQ-89NFbxTUscd2Lv_7lCBstnZsrYBSq'; // You need to replace it with your actual server key
 
   async sendNotification(
     token: string,
