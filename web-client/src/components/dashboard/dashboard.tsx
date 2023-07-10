c14aa4066b833d86256c70b34aa8f4dd9bba0839
18848 <a18848@alunos.ipca.pt>
2023-07-10 01:53:45 +0100

    dashboard

--- web-client/src/components/dashboard/dashboard.tsx
+++ web-client/src/components/dashboard/dashboard.tsx
@@ -1,20 +1,74 @@
-import React from 'react';
+import { Card, Col, Divider, List, Progress, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 const Dashboard: React.FC = () => {
+    const [totalSales, setTotalSales] = useState<string>();
+    const [reservations, setReservations] = useState<string>();
+    const [transactions, setTransactions] = useState<string>();
+    const [operationalSlots, setOperationalSlots] = useState<string>();
+    const [topStations, setTopStations] = useState<any>();
+
+    useEffect(() => {
+        setTotalSales('1000')
+        setReservations('10000')
+        setTransactions('1000')
+        setOperationalSlots('90')
+    }, [totalSales]);
+    
     return (
         <div className='dashboard-container'>
-            <div className='dashboard-header'>
-                <h2>Dashboard</h2>
-            </div>
-            <div className='dashboard-cards'>
-                <div className='dashboard-card-sales'></div>
-                <div className='dashboard-card-reservations'></div>
-                <div className='dashboard-card-transactions'></div>
-                <div className='dashboard-card-slots'></div>
-            </div>
+            <Row className='dashboard-header' gutter={16}>
+                <Col span={6}>
+                    <Card title="Total Sales"
+                    size='small' headStyle={{ textAlign: 'start' }}
+                    className='dashboard-card' bordered={false} style={{ width: 300 }}>
+                        <h3 className='card-content'>$ {totalSales}</h3>
+                        <br/>
+                    </Card>
+                </Col>
+                <Col span={6}>
+                    <Card title="Reservations"
+                        size='small' headStyle={{ textAlign: 'start' }}
+                        className='.dashboard-card' bordered={false} style={{ width: 300 }}>
+                            <h3>{reservations}</h3>
+                            <br/>
+                        </Card>
+                </Col>
+                <Col span={6}>
+                    <Card title="Transactions"
+                        size='small' headStyle={{ textAlign: 'start' }}
+                        className='.dashboard-card' bordered={false} style={{ width: 300 }}>
+                            <h3>{transactions}</h3>
+                            <br/>
+                        </Card>
+                </Col>
+                <Col span={6}>
+                    <Card title="Operational Slots"
+                        size='small' headStyle={{ textAlign: 'start' }}
+                        className='.dashboard-card' bordered={false} style={{ width: 300 }}>
+                            <h3>{operationalSlots}%</h3>
+                            <Progress percent={Number.parseInt(operationalSlots!)} showInfo={false} />
+                        </Card>
+                </Col>
+            </Row>
+            <br/>
             <div className='dashboard-graph'>
-                <div className='dashboard-graph-sales'></div>
-                <div className='dashboard-graph-visits'></div>
+                <Row className='dashboard-header' gutter={16}>
+                    <Col span={6}></Col>
+                    <Col span={6}></Col>
+                <div className='dashboard-graph-bars'></div>
+                    <Col span={6}></Col>
+                    <Col span={6}>
+                        <List style={{ width: 300 }}
+                        size="small"
+                        header={<div>Sales Ranking</div>}
+                        bordered
+                        dataSource={topStations}
+                        renderItem={(item: any) => <List.Item>{item}</List.Item>}
+                        />
+                    </Col>
+                </Row>
             </div>
         </div>
     );
